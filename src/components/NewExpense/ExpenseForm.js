115540f45030449e9bcfd726d22d99239ce281c9
SNOW <kancth03@gmail.com>
2023-03-09 02:03:59 +0900

    ExpenseForm Submit 데이터 보내기 구현
    ExpenseForm에서 기본 데이터 입력 후 Submit시 입력값을 expenseData로 return하도록 함

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+    // })
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandler = event => {
+        setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value};
+        // });
+    };
+
+    const amountChangeHandler = event => {
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+    };
+
+    const dateChangeHandler = event => {
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+    };
+
     return (
-        <form action="">
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>내용</label>
-                <input type="text"/>
+        <form onClick={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>내용</label>
+                    <input type="text" onChange={titleChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>금액</label>
+                    <input type="number" min="0" step="1" onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>날짜</label>
+                    <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
+                </div>
             </div>
-            <div className="new-expense__control">
-                <label>금액</label>
-                <input type="number" min="0" step="1"/>
-            </div>
-            <div className="new-expense__control">
-                <label>날짜</label>
-                <input type="date" min="2019-01-01" max="2023-12-31"/>
-            </div>
-        </div>
-            <div className="new-expense__actions">
-                <button type="button">내역 추가</button>
-            </div>
+                <div className="new-expense__actions">
+                    <button type="submit">내역 추가</button>
+                </div>
     </form>
     )
 };
