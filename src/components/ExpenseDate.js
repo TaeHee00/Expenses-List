d50f028401ec6038adb0fdaf7e4688737db10dc7
SNOW <kancth03@gmail.com>
2023-03-08 18:23:57 +0900

    Card Component 추가
    Expense items를 한번에 조작하기위해 컴포넌트 추가

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -6,8 +6,8 @@ function ExpenseDate(props){
     const year = props.date.getFullYear();
 
     return <div className="expense-date">
-        <div className="expense-date__month">{year}년</div>
-        <div className="expense-date__year">{month}</div>
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}년s</div>
         <div className="expense-date__day">{day}</div>
     </div>;
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,17 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 function ExpenseItem(props){
-    const expenseTitle = props.title;
-    const expenseAmount = props.amount;
-
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             {/*강효원 권범수*/}
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className={"expense-item__price"}>{expenseAmount}원</div>
+                <h2>{props.title}</h2>
+                <div className={"expense-item__price"}>{props.amount.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
