5c53878b2ed5e347fc36a34f300ff816d9b46ba2
SNOW <kancth03@gmail.com>
2023-03-08 23:57:46 +0900

    item title 수정 버튼 생성
    ExpenseItem.js file에 useState import 및 기본적인 틀 구현

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,24 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Update!')
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
-            {/*강효원 권범수*/}
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className={"expense-item__price"}>{props.amount.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원</div>
             </div>
+            <button onClick={clickHandler}>Title Change</button>
         </Card>
     );
 }
