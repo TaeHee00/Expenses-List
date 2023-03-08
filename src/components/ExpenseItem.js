3d6d21afb70a046762892b76cad7791be664528e
SNOW <kancth03@gmail.com>
2023-03-08 17:14:50 +0900

    ExpenseItem div date 수정
    ExpenseItem div list에서 날짜를 한국어로 변경 후 재사용성을 높이기 위한 변수 처리

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,9 +5,16 @@ function ExpenseItem(props){
     const expenseTitle = props.title;
     const expenseAmount = props.amount;
 
+    const month = props.date.toLocaleString('ko-KR', {month: 'long'});
+    const day = props.date.toLocaleString('ko-KR', {day: '2-digit'});
+    const year = props.date.getFullYear();
+
     return (
         <div className="expense-item">
-            <div>{expenseData.toDateString()}</div>
+            <div>
+                <div>{year}년 {month} {day}</div>
+                {/*강효원 권범수*/}
+            </div>
             <div className="expense-item__description">
                 <h2>{expenseTitle}</h2>
                 <div className={"expense-item__price"}>${expenseAmount}</div>
